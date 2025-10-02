e7e9964a71b384006be3e6d7cd14173b611dd10e
Sergey Brovko <serbro7@gmail.com>
2025-10-02 17:13:46 +0200

    combining XML with component scan of beans with annotations

--- src/main/java/com/in28minutes/spring/basics/spring_in_5_steps/SpringIn5StepsXmlContextApplication.java
+++ src/main/java/com/in28minutes/spring/basics/spring_in_5_steps/SpringIn5StepsXmlContextApplication.java
@@ -1,27 +1,30 @@
 package com.in28minutes.spring.basics.spring_in_5_steps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.in28minutes.spring.basics.spring_in_5_steps.basic.BinarySearchImpl;
 import com.in28minutes.spring.basics.spring_in_5_steps.xml.XMLPersonDAO;
 
-@Configuration
-@ComponentScan
 public class SpringIn5StepsXmlContextApplication {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
 
 	public static void main(String[] args) {
 				
 		try (var applicationContext = 
 				new ClassPathXmlApplicationContext("applicationContext.xml")) {
 			
+			LOGGER.info("Beans Loaded -> {}", 
+					(Object)applicationContext.getBeanDefinitionNames());
+			
 			var personDao = 
 					applicationContext.getBean(XMLPersonDAO.class);
 			System.out.println(personDao);
 			System.out.println(personDao.getXMLJdbcConnection());
+			
+			LOGGER.info("{} {}", personDao, personDao.getXMLJdbcConnection());
 			
 		} catch (BeansException e) {
 			e.printStackTrace();

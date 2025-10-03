7617070272e503af503e99bdbd51b74667c4c918
Sergey Brovko <serbro7@gmail.com>
2025-10-03 16:01:28 +0200

    Preparation for JUnit: remove spring boot test dependency, and put in basic junit

--- src/test/java/com/in28minutes/spring/basics/spring_in_5_steps/SpringIn5StepsBasicApplicationTests.java
+++ src/test/java/com/in28minutes/spring/basics/spring_in_5_steps/SpringIn5StepsBasicApplicationTests.java
@@ -1,9 +1,8 @@
 package com.in28minutes.spring.basics.spring_in_5_steps;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = SpringIn5StepsBasicApplication.class)
+//@SpringBootTest(classes = SpringIn5StepsBasicApplication.class)
 class SpringIn5StepsBasicApplicationTests {
 
 	@Test

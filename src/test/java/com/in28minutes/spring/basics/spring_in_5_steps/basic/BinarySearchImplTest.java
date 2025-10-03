9e4d6e041c787654eba71d27d3f22cba2d46a7da
Sergey Brovko <serbro7@gmail.com>
2025-10-03 16:47:16 +0200

    JUnit with Java Context

---
+++ src/test/java/com/in28minutes/spring/basics/spring_in_5_steps/basic/BinarySearchImplTest.java
@@ -0,0 +1,31 @@
+package com.in28minutes.spring.basics.spring_in_5_steps.basic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.in28minutes.spring.basics.spring_in_5_steps.SpringIn5StepsBasicApplication;
+
+//Load the context. The tutorial uses SpringRunner, but it doesn't work. Perhaps it's deprecated? I used SpringExtension instead
+@ExtendWith(SpringExtension.class)
+//Configuration for the context
+@ContextConfiguration(classes=SpringIn5StepsBasicApplication.class)
+class BinarySearchImplTest {
+	
+	//Get this bean from the context
+	@Autowired
+	BinarySearchImpl binarySearch;
+
+	@Test
+	void Should_ReturnTrue_InBasicScenario() {
+		//call method on binarysearch
+		var actualResult = binarySearch.binarySearch(new int[] {}, 5);
+		//check if the value is correct
+		assertEquals(42, actualResult);
+	}
+
+}
